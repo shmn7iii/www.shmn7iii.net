f1943b6c557b18eabe7bf41ec5992e0193989b9b
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-03-24 16:10:30 +0900

    style: change header icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,10 @@
-import MoodSmile from "https://deno.land/x/tabler_icons_tsx@0.0.6/tsx/mood-smile.tsx";
-
 type Props = {
   active: string;
 };
 
 export default function Header({ active }: Props) {
   const menus = [
+    { name: "Home", href: "/" },
     { name: "About", href: "/about" },
     { name: "Works", href: "/works" },
     { name: "Blog ↗︎", href: "https://blog.shmn7iii.net" },
@@ -15,8 +14,13 @@ export default function Header({ active }: Props) {
     <header className="w-full">
       <div className="flex flex-col md:flex-row justify-between gap-4">
         <a href="/">
-          <div className="flex items-center gap-1">
-            <MoodSmile aria-hidden="true" />
+          <div className="flex items-center gap-2">
+            <img
+              src="/icon.png"
+              width="26"
+              height="26"
+              alt="shmn7iii icon"
+            />
             <div className="text-2xl ml-1 font-bold">
               www.shmn7iii.net
             </div>
