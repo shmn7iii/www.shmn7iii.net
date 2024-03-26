aee67d8cce818890527db7cffb1ea178e55e95d7
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-03-27 00:39:17 +0900

    fix: fix image path of root page

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <>
       <img
-        src="/icon.png"
+        src="/icon-shmn7iii.jpg"
         alt="shmn7iii icon"
-        className="w-32 animate-bounce"
+        className="w-32 rounded-full animate-bounce"
       />
       <h3 className="text-2xl font-bold mb-5">
         metyakutya wip
