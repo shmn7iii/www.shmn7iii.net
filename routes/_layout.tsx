b853f3dad1b85166f651d00b7c4746a6f9127a8d
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2024-03-27 02:59:01 +0900

    style: change px; responsive

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header.tsx";
 
 export default function Layout({ Component, route }: PageProps) {
   return (
-    <div className="flex flex-col items-center min-h-dvh px-12 py-6 lg:px-44">
+    <div className="flex flex-col items-center min-h-dvh px-5 py-6 md:px-12 lg:px-44">
       <Header active={route} />
       <main className="h-full w-full max-w-4xl py-12 flex flex-col items-center md:mt-10">
         <Component />
