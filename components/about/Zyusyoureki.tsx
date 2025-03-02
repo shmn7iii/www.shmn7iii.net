b64995db6cfdf00626b496a34bef0e5d35d9dd3a
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2025-03-02 14:03:30 +0900

    remove: delete old project

--- components/about/Zyusyoureki.tsx
+++
@@ -1,8 +0,0 @@
-import Timeline from "./Timeline.tsx";
-import zyusyourekis from "../../data/about/zyusyoureki.json" with {
-  type: "json",
-};
-
-export default function Zyusyoureki() {
-  return <Timeline title="受賞歴" items={zyusyourekis} />;
-}
--- routes/posts/_middleware.ts
+++
@@ -1,6 +0,0 @@
-export function handler(req: Request) {
-  const requestUrl = new URL(req.url);
-  const redirectUrl = Deno.env.get("BLOG_REDIRECT_URL") + requestUrl.pathname;
-
-  return Response.redirect(redirectUrl, 301);
-}
