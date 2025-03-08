887ff68ddaeff126e478d8bdfb5a01dd96c37b28
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2025-03-08 12:47:12 +0900

    fix: font loading in html

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -27,6 +27,10 @@ export default function App({ Component }: PageProps) {
         <meta name="twitter:image" content={ogpImage} />
 
         <link rel="stylesheet" href="/styles.css" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet" />
       </head>
       <body class="bg-[#e3ded2]">
         <Component />
