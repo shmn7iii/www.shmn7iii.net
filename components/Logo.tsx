edf1de18492934e3183696f37b0ce1f582509989
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2025-05-17 23:05:10 +0900

    update: logo a tag

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,9 +1,11 @@
 export default function Logo() {
   return (
-    <div class="w-fit pt-2 px-4 mb-6 bg-[#2d2d2d]">
-      <a href='/' class="font-josefinsans font-medium text-2xl text-[#e3ded2]">
-        shmn7iii.net
-      </a>
-    </div>
+    <a href="/">
+      <div class="w-fit pt-2 px-4 mb-6 bg-[#2d2d2d]">
+        <p class="font-josefinsans font-medium text-2xl text-[#e3ded2]">
+          shmn7iii.net
+        </p>
+      </div>
+    </a>
   );
 }
